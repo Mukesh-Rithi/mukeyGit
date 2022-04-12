e0ea1e5201f7591708ef76c42fa711e6241f2646
Mukesh_Rithi <mukeshwaran11108@gmail.com>
2022-04-11 21:24:54 -0700

    Mikey

---
+++ src/org/tcs/Sample.java
@@ -0,0 +1,27 @@
+package org.tcs;
+
+public class Sample {
+
+	private void raiden() {
+
+		System.out.println("Dinesh");
+		
+	}
+	private void wolf() {
+
+		System.out.println("Karthi");
+	}
+	private void karma() {
+
+		System.out.println("mukesh");
+	}
+	public static void main(String[] args) {
+		 
+		Sample e = new Sample();
+		
+		e.raiden();
+		e.wolf();
+		e.karma();
+	
+	}
+}
